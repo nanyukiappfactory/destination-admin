cdb0ed36387863ba79a69991bd1195af9c3009d2
kwizl <karis.njoroge@gmail.com>
2019-03-12 14:46:53 +0300

    added validation logic

--- application/modules/Auth/controllers/Auth.php
+++ application/modules/Auth/controllers/Auth.php
@@ -4,7 +4,30 @@
         function __construct()
         {
             parent:: __construct();
+
+            $this->load->model('auth_model');
         }
 
+        public function index()
+        {
+            $this->form_validation->set_rules('admin_name', '', 'required');
+            $this->form_validation->set_rules('admin_password', '', 'required');
+
+            if($this->form_validation->run() == TRUE)
+            {
+                $admin_username = $this->input->post('admin_name');
+                $admin_password= md5($this->input->post('admin_password'));
+
+                if($this->auth_model->validate_administrator($username, $password) == TRUE)
+                {
+                    redirect('administration');
+                }
+                else
+                {
+                    $this->session->set_flashdata('error', 'Incorrect Username or Password');
+                    redirect('login');
+                }
+            }
+        }
     }
 ?>
--- application/modules/Auth/models/Auth_model.php
+++ application/modules/Auth/models/Auth_model.php
@@ -2,7 +2,34 @@
 
     class Auth_model extends CI_Model
     {
-        
+        public function validate_administrator($usernaem, $password)
+        {
+            if($username == "admin" && $password == md5(123456))
+            {
+                $sess_array = array(
+                    'username' => $username,
+                    'login_status' => TRUE
+                );
+                $this->session->set_userdata('login_status', $sess_array);
+                return TRUE;
+            }
+            else
+            {
+                return FALSE;
+            }
+        }
+
+        public function check_login_status()
+        {
+            if($this->session->userdata('login_status'))
+            {
+                return TRUE;
+            }
+            else
+            {
+                return FALSE;
+            }
+        }
     }
 
 ?>
