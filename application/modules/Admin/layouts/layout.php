ba2e62227101c008dd4b552c6a10e9a90693c45e
cecilia <cessygitahi57@gmail.com>
2019-03-12 15:34:23 +0300

    layouts changes

--- application/modules/Admin/layouts/layout.php
+++ application/modules/Admin/layouts/layout.php
@@ -1,23 +1,34 @@
-<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-    
+<!DOCTYPE html>
+<html lang="en">
 
-      <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+<?php $this->load->view('layouts/header');?>
+<?php $this->load->view('layouts/navigation');?>
+<?php $this->load->view('layouts/sidebar');?>
+<?php $this->load->view('layouts/footer');?>
 
-      <h2>Section title</h2>
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Header</th>
-              <th>Header</th>
-              <th>Header</th>
-              <th>Header</th>
-            </tr>
-          </thead>
-          <tbody>
-            
-          </tbody>
-        </table>
-      </div>
-    </main>
+<body>
+	<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+
+
+		<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+
+		<h2>Section title</h2>
+		<div class="table-responsive">
+			<table class="table table-striped table-sm">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Header</th>
+						<th>Header</th>
+						<th>Header</th>
+						<th>Header</th>
+					</tr>
+				</thead>
+				<tbody>
+
+				</tbody>
+			</table>
+		</div>
+	</main>
+</body>
+</html>
