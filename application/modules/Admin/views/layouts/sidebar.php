5cb571ac4d0cd53bd5c37705942bfeb399812cce
cecilia <cessygitahi57@gmail.com>
2019-03-13 17:13:59 +0300

    made layout changes

--- application/modules/Admin/views/layouts/sidebar.php
+++ application/modules/Admin/views/layouts/sidebar.php
@@ -1,5 +1,6 @@
+
 <div class="container-fluid">
-  	<div class="row">
+	<div class="row">
 		<nav class="col-md-2 d-none d-md-block bg-light sidebar">
 			<div class="sidebar-sticky">
 				<ul class="nav flex-column">
@@ -28,5 +29,3 @@
 		</nav>
 	</div>
 </div>
-
-
