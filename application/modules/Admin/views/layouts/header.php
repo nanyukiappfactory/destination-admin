6d51f71a8f115a96f37dc3e642c15966a240305d
cecilia <cessygitahi57@gmail.com>
2019-03-13 10:34:29 +0300

    removed styling links

--- application/modules/Admin/views/layouts/header.php
+++ application/modules/Admin/views/layouts/header.php
@@ -1,11 +1,5 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.5">
-    <title>Destination Laikipia</title>
+ <head> 
+    <title><?php echo $title;?></title>
     <link href="<?php echo base_url();?>assets/custom/themes/vendor/bootstrap/css/dashboard.css" rel="stylesheet">
     <link href="<?php echo base_url();?>assets/custom/themes/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  </head>
+ </head>
