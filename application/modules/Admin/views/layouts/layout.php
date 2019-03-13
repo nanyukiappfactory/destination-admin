d5487b443212cdf47ae550c3bd7ce8f912700184
kwizl <karis.njoroge@gmail.com>
2019-03-13 16:17:58 +0300

    added business type content

--- application/modules/Admin/views/layouts/layout.php
+++ application/modules/Admin/views/layouts/layout.php
@@ -3,14 +3,26 @@
 
 	<?php $this->load->view('admin/layouts/header');?>
     <body>
+	<div class="d-flex">
 
-        <?php $this->load->view('admin/layouts/navigation');?>
+		<?php $this->load->view('admin/layouts/sidebar');?>
 
-        <?php $this->load->view('admin/layouts/sidebar');?>
-		
-		<?php echo $content;?>
+		<!-- Page Content -->
+		<div id="page-content-wrapper">
+
+			<?php $this->load->view('admin/layouts/navigation');?>
+
+		<div class="container-fluid">
+			
+		</div>
+	</div>
+<!-- /#page-content-wrapper -->
+
+
 		
 		<?php $this->load->view('admin/layouts/footer');?>
 
 	</body>
+
+	
 </html>
