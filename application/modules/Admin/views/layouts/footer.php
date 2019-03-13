b731c91ad1c10fd73d70e65231476266da1cd8db
cecilia <cessygitahi57@gmail.com>
2019-03-13 11:41:33 +0300

    added footer changes

--- application/modules/Admin/views/layouts/footer.php
+++ application/modules/Admin/views/layouts/footer.php
@@ -2,4 +2,4 @@
 <script src="<?php echo base_url();?>assets/custom/themes/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo base_url();?>assets/custom/themes/vendor/bootstrap/js/feather.min.js"></script>
 <script src="<?php echo base_url();?>assets/custom/themes/vendor/bootstrap/js/Chart.min.js"></script>
-<script src="<?php echo base_url();?>assets/custom/themes/vendor/bootstrap/js/dashboard.js"></script></body>
+<script src="<?php echo base_url();?>assets/custom/themes/vendor/bootstrap/js/dashboard.js"></script>
