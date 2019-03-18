21b9d3fd11a53c5d171f2c29e87d5e9942383fb7
kwizl <karis.njoroge@gmail.com>
2019-03-18 08:24:06 +0300

    added view, pagination and sorting

--- application/modules/Admin/controllers/Proprietors.php
+++ application/modules/Admin/controllers/Proprietors.php
@@ -46,8 +46,10 @@ class Proprietors extends admin
         $v_data['order_method'] = $order_method;
         $v_data['order'] = $order;
         $v_data['counter'] = $page * $limit_per_page;
+
         //Assign view as string with no data to var $content
         $data['content'] = $this->load->view('proprietor/all_proprietors', $v_data, true);
+        
         //check and change order method
         $data['route'] = 'proprietors';
         $first_name = array();
