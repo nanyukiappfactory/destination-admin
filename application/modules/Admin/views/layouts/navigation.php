7984a28294955806686cf30d14a6f673d12b8303
cecilia <cessygitahi57@gmail.com>
2019-03-18 15:25:30 +0300

    solving  navigation search

--- application/modules/Admin/views/layouts/navigation.php
+++ application/modules/Admin/views/layouts/navigation.php
@@ -2,6 +2,7 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Destination Laikipia</a>
   <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
     <?php echo form_open(base_url() . $route.'/search-' . $route, array("class" => "form-inline my-2 my-lg-0"));?>
       <?php 
           foreach($search_options as $search_option_array)
@@ -21,7 +22,6 @@
       <?php }?>
       <a class="nav-link" href="<?php echo base_url();?>admin/logout">Sign out</a>
   <?php echo  form_close();?>
-    <li class="nav-item text-nowrap">
     
     </li>
   </ul>
